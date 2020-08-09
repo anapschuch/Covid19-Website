0c2c4bd14ba3be7ccdfb71912dab4836501aa391
Ana Paula Schuch <aplschuch@gmail.com>
2020-08-09 00:09:31 -0300

    Adicionado paginação

--- Django/settings.py
+++ Django/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'covid19.projetoces22@gmail.com'
-EMAIL_HOST_PASSWORD = 'ITACES22'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Paciente
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
 def patient_edit(request):
     """Returns the patient editor."""
     pesquisar = request.GET.get('pesquisar')
+
+    if pesquisar is not None:
+        if len(pesquisar) == 0:
+            pesquisar = None
+
     if pesquisar is not None:
         pacientes = Paciente.objects.filter(nome__startswith=pesquisar).order_by('nome')
-        if len(pesquisar) == 0:
-            pesquisar = "Nome"
     else:
         pacientes = Paciente.objects.all().order_by('nome')
         pesquisar = "Nome"
+        paginator = Paginator(pacientes, 15)
+        page = request.GET.get('page', 1)
+
+        try:
+            pacientes = paginator.page(page)
+        except PageNotAnInteger:
+            pacientes = paginator.page(1)
+        except EmptyPage:
+            pacientes = paginator.page(paginator.num_pages)
+
     return render(request, 'patient_edit.html', {'pacientes': pacientes, 'pesquisar': pesquisar})
 
 
